39f30b99b04e17cdb971fd3ca60a1ddb4d4dc528
Shock3600 <dalston.levi.jones@gmail.com>
2025-10-14 02:23:00 -0500

    Added trainer pool for LC trainers

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -858,12 +858,17 @@
 #define TRAINER_LEAF                        852
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
+#define TRAINER_LC_2		                855
+#define TRAINER_LC_3		                856
+#define TRAINER_LC_4		                857
+#define TRAINER_LC_5		                858
+#define TRAINER_LC_6		                859
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      855
+#define TRAINERS_COUNT                      860
 #define MAX_TRAINERS_COUNT                  864
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
