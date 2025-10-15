be5d5afcaadf48304877334d30c33614c61e7306
Shock3600 <dalston.levi.jones@gmail.com>
2025-10-14 23:32:45 -0500

    Changed squirtle starter to pichu

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -113,7 +113,7 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
     SPECIES_DRATINI,
-    SPECIES_SQUIRTLE,
+    SPECIES_PICHU,
     SPECIES_ODDISH,
 };
 
