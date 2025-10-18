d57a0ca810aaf3bea3c862ef987d528562c828ea
Shock3600 <dalston.levi.jones@gmail.com>
2025-10-17 19:05:25 -0500

    Changed starters to bagon, horsea, oddish temporarily for the little cup demo

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -112,8 +112,8 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_DRATINI,
-    SPECIES_PICHU,
+    SPECIES_BAGON,
+    SPECIES_HORSEA,
     SPECIES_ODDISH,
 };
 
